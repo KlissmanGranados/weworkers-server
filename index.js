438644bbd510fec25af5e34055668d25a7decb21
Klissman Granados <www.klissman@gmail.com>
2021-09-02 03:31:02 -0400

    cabeceras

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
+
 app.use(express.json());
 module.exports = app;
 
