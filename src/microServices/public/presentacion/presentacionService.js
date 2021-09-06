512632f7da31cdfa1d72e8531db67ce278374979
Klissman Granados <www.klissman@gmail.com>
2021-09-06 15:21:35 -0400

    Listar rutas #8

--- src/microServices/public/presentacion/presentacionService.js
+++ src/microServices/public/presentacion/presentacionService.js
@@ -3,9 +3,15 @@ const {app} = require('../../../../');
 const {getRoutes} = require('get-routes');
 
 exports.main = (req, res) => {
-  const host = process.env.HOST || 'http://localhost';
-  const port = process.env.PORT || 3000;
+  let host = process.env.HOST || 'http://localhost:3000';
+  host = host != 'http://localhost'? host: host + `:${process.env.PORT || 3000}`;
   const routes = getRoutes(app);
-  const data = {host, port, routes};
-  response.routes_listing(res, data);
+
+  routes.post = routes.post.map((route) => host + route);
+  routes.get = routes.get.map((route) => host + route);
+  routes.put = routes.put.map((route) => host + route);
+  routes.delete = routes.delete.map((route) => host + route);
+  routes.patch = routes.patch.map((route) => host + route);
+
+  response.routes_listing(res, routes);
 };
