d9ee0b61d917657a83d67dbb37fde0579da7f43e
Klissman Granados <www.klissman@gmail.com>
2021-09-22 04:32:42 -0400

    insertando proyectos #15

--- src/microServices/captador/projectManagement/projectManagementMiddleware.js
+++ src/microServices/captador/projectManagement/projectManagementMiddleware.js
@@ -8,5 +8,47 @@ const {Proyecto,Tag,ProyectoTag} = require('../../../Entities')
  * @param {NextFunction} next 
  */
 exports.create = (req,res,next)=>{
+  
+  const {proyecto,tags} = req.body;
+  const _proyecto = new Proyecto();
+  let requireInputs = [];
+  
+  // verificar json del cliente
+  if(!proyecto || !tags) {
+    response.warning_required_fields(
+      res,{proyecto:proyecto||null,tags: tags||null}
+    )
+    return;
+  }
+  // cargar etiquetas
+  const _tags = tags.map( (tag) => {
+    const _tag = new Tag();
+    _tag.loadData(tag);
+    requireInputs = requireInputs.concat(
+      _tag.checkRequired(['nombre'])
+    );
+    return _tag; 
+  });
+  // verificar si hay etiquetas vacias
+  if(requireInputs.length > 0){
+    response.warning_required_fields(res,{tags:requireInputs});
+    return;
+  }
+  _proyecto.loadData(proyecto);
+  // verificar si los datos del proyectos estan siendo enviados
+  requireInputs = requireInputs.concat(
+    _proyecto.checkRequired(['nombre','descripcion'])
+  );
+  if(requireInputs.length > 0){
+    response.warning_required_fields(res,{proyecto:requireInputs});
+    return;
+  }
+  // setear datos para manejarlos en el servicio
+  req.registro = {
+    proyecto : _proyecto,
+    tags : _tags,
+    proyectoTags: new ProyectoTag(),
+  }
+
   next();
 }
--- src/microServices/captador/projectManagement/projectManagementService.js
+++ src/microServices/captador/projectManagement/projectManagementService.js
@@ -6,6 +6,40 @@ const projectManagementRepository = require('./projectManagementRepository');
  * @param {Request} req 
  * @param {Response} res 
  */
-exports.create = (req,res)=>{
+exports.create = async (req,res)=>{
+  const registro = req.registro;
+  registro.tags = await projectManagementRepository
+  .findTagByName(registro.tags);
   
+  const _proyectoTag = registro.proyectoTags.clone();
+  // setear los tags existentes
+  registro.proyectoTags = registro.tags.map((tag)=> {
+    if(tag.id){
+      const proyectosTags = _proyectoTag.clone();
+      proyectosTags.tagsId = tag.id;
+      return proyectosTags;
+    }
+  });
+  // setear los tags no existentes
+  registro.tags = registro.tags.filter((tag)=>!tag.id);
+  // Crear etiquetas no existentes
+  registro.tags = await projectManagementRepository.createTags(registro.tags);
+  // setear ids de los tags agregados
+  registro.tags.forEach( tag =>{
+    const proyectosTags = _proyectoTag.clone();
+    proyectosTags.tagsId = tag.id;
+    registro.proyectoTags.push(proyectosTags);
+  });
+  registro.tags = undefined;
+  
+  registro.proyecto.reclutadoresId = await projectManagementRepository
+    .findReclutadorByUserId(req.user.idusuario);
+  
+  const check = await projectManagementRepository.create(registro);
+
+  if(!check){
+    response.error(res);
+    return;
+  }
+  response.success(res);
 };
