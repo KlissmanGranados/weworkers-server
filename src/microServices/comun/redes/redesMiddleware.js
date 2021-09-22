3e7be969a444e451bea407121e8d5998cd22b9fe
Klissman Granados <www.klissman@gmail.com>
2021-09-22 19:35:19 -0400

    cargando estructura #6

--- src/microServices/comun/redes/redesMiddleware.js
+++ src/microServices/comun/redes/redesMiddleware.js
@@ -1,4 +1,31 @@
+const response = require('../../../response');
 const {Red,RedDireccion,DireccionRedUsuario} = require('../../../Entities');
+
+/**
+ * @description Crea una asociación entre usuario y red
+ * @param {Request} req 
+ * @param {Response} res 
+ * @param {NextFunction} next 
+ */
 exports.AssociateNetwork = (req,res,next)=>{
+  
+  const {redId,direccion} = req.body;
+
+  if(!redId){
+    response.warning_required_fields({redId:null});
+    return;
+  }
+  if(!direccion){
+    response.warning_required_fields({direccion:null});
+    return;
+  }
+
+  const red = new Red();
+  const redDireccion = new RedDireccion();
+  const direccionRedUsuario = new DireccionRedUsuario();
+  
+  red.id = redId;
+  redDireccion.direccion = direccion;
+  
   next();
 }
--- src/microServices/comun/redes/redesService.js
+++ src/microServices/comun/redes/redesService.js
@@ -1,5 +1,5 @@
 const response = require('../../../response');
-
+const redesRepository = require('./redesRepository');
 /**
  * @description vincular red con usuario
  * @param{Request} req
