a78bd9e0e7bef6c40bff0660e5d2d6df2339a43f
Klissman Granados <www.klissman@gmail.com>
2021-10-13 00:45:56 -0400

    agregando token a headers

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ consts();
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   // eslint-disable-next-line max-len
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, token');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
