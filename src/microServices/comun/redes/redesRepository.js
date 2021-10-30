a210afb4e9d81a2e7a126794013f9f512c414fe0
KlissmanGranados <granadosklissman@gmail.com>
2021-10-30 03:02:13 -0400

    - Agregando informacion al seleccionar perfiles - Arreglando bug de delete en redes - Incorporando restricción para evitar que el usuario tenga n cuentas asociadas en codewars (puede agregarse mas redes con esta condicion) - Incorporando totalPages en response/repage.js

--- src/microServices/comun/redes/redesRepository.js
+++ src/microServices/comun/redes/redesRepository.js
@@ -45,17 +45,39 @@ exports.update = (redDireccion)=>{
   });
 };
 /**
- *@description Elimina una una red
+ *@description Elimina una una red asociada a un usuario
  * @param { { redDireccion:RedDireccion } } redDireccion
  * @return {Promise<Boolean>}
  */
 exports.delete = (redDireccion)=>{
   return db.transaction(async (conn)=>{
-    await conn.query(
-        `DELETE FROM redes_usuarios WHERE redes_direcciones_id=$1`,
-        [redDireccion.id],
-    );
-    const {rowCount} = await conn.query(redDireccion.delete());
+    const id = (await conn.query(
+        `DELETE FROM redes_usuarios WHERE id=$1 
+        RETURNING redes_direcciones_id`,
+        [redDireccion.id])).rows[0].redes_direcciones_id;
+    const {rowCount} = await conn.query(`
+      DELETE FROM redes_direcciones WHERE id=$1
+    `, [id]);
+    return rowCount > 0;
+  });
+};
+/**
+ * verifica si hay una red que solo se pueda registrar una vez,
+ * es decir, en caso de ser necesario ese dato para una operacion,
+ * se puede usar << checkOnlyOne >> para evitar que un usuario
+ * tenga varias cuentas en una plataforma.
+ * @param {BigInteger} redId
+ * @param {BigInteger} userId
+ * @return {Promise<Boolean>}
+ */
+exports.checkOnlyOne = (redId, userId)=>{
+  return db.execute(async (conn) => {
+    const {rowCount} = await conn.query(`
+      SELECT redes_direcciones.redes_id FROM redes_usuarios 
+      JOIN redes_direcciones ON 
+      redes_direcciones.id = redes_usuarios.redes_direcciones_id
+      WHERE redes_usuarios.usuario_id=$1 and redes_direcciones.redes_id=$2    
+    `, [userId, redId]);
     return rowCount > 0;
   });
 };
