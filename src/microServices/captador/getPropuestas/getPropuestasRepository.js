ddd3e736bf5cb6496854aced46ab71c357631c76
KlissmanGranados <granadosklissman@gmail.com>
2021-11-08 09:40:08 -0400

    proposals repage

--- src/microServices/captador/getPropuestas/getPropuestasRepository.js
+++ src/microServices/captador/getPropuestas/getPropuestasRepository.js
@@ -2,85 +2,42 @@ const {db} = require('../../../../index');
 
 /**
  * @description devuelve las propuestas de un proyecto
- * basados en un limit y un offset
- * @param {Number} idProyecto
- * @param {Number} limit
- * @param {Number} offset
- * @return {Promise<Array>}
+ * @param {Request} req
+ * @return {Promise}
  */
-exports.getPropuestas = async (idProyecto, limit, offset) => {
-  return db.execute(async (conn) => {
-    const query = await conn.query(`
-    SELECT personas.primer_nombre, personas.primer_apellido,
-    personas.id as personasid, usuarios.usuario, 
-    usuarios.id as usuariosid, 
-    proyectos_propuestas.id as propuestasid,
-     proyectos_propuestas.mensaje
-    FROM usuarios INNER JOIN personas ON 
-    (usuarios.persona_id=personas.id)
-    INNER JOIN trabajadores ON
-    (usuarios.id=trabajadores.usuarios_id)
-    INNER JOIN proyectos_propuestas ON
-    (proyectos_propuestas.trabajadores_id=trabajadores.id)
+exports.getPropuestas = async (req) => {
+  const {perPage, page, idProyecto} = req.query;
+  return db.repage({
+    limit: {
+      offset: page || 0,
+      rowsLimit: perPage || 20,
+    },
+    text: `
+    SELECT 
+    proyectos_propuestas.id,
+    proyectos_propuestas.mensaje,
+    proyectos_propuestas."timestamp",
+    usuarios.id AS usuario_id,
+    tipos_identificacion.tipo AS tipo_identificacion,
+    personas.identificacion ,
+    personas.primer_nombre,
+    personas.primer_apellido,
+    personas.segundo_nombre,
+    personas.segundo_apellido 
+    FROM proyectos_propuestas
+    JOIN trabajadores 
+    ON trabajadores.id = proyectos_propuestas.trabajadores_id
+    JOIN usuarios 
+    ON usuarios.id = trabajadores.usuarios_id
+    JOIN personas 
+    ON personas.id = usuarios.persona_id
+    JOIN tipos_identificacion 
+    ON tipos_identificacion.id = personas.id_tipo_identificacion 
     WHERE proyectos_propuestas.proyectos_id=$1
-    ORDER BY proyectos_propuestas."timestamp"
-    LIMIT $2 OFFSET $3
-    `, [idProyecto, limit, offset]);
-
-    return query.rows;
-  });
-};
-/**
- * @description devuelve el número de propuestas que se han hecho a un proyecto
- * @param {Number} idProyecto
- * @return {Promise<Number>}
- */
-exports.countPropuestas = async (idProyecto) =>{
-  return db.execute( async (conn) => {
-    const counter = await conn.query(`SELECT id FROM proyectos_propuestas 
-        WHERE proyectos_id=$1`, [idProyecto]);
-
-    return counter.rowCount;
-  });
-};
-/**
- * @description determina si un usuario ha metido una propuesta a un proyecto
- * @param {Number} idUsuario
- * @param {Number} idProyecto
- * @return {Promise<Object>}
- */
-exports.isUserInProposals = async (idUsuario, idProyecto) =>{
-  return db.transaction( async (conn) => {
-    const check = await conn.query(`SELECT proyectos_propuestas.id,
-    usuarios.usuario 
-    FROM usuarios INNER JOIN trabajadores ON
-    (trabajadores.usuarios_id=usuarios.id) INNER JOIN
-    proyectos_propuestas 
-    ON (proyectos_propuestas.trabajadores_id=trabajadores.id)
-    WHERE usuarios.id=$1 AND 
-    proyectos_propuestas.proyectos_id=$2`, [idUsuario, idProyecto]);
-
-    return check.rows[0];
-  });
-};
-
-exports.getUsuarioPropuesta = async (idPropuesta) => {
-  return db.execute(async (conn) => {
-    const query = await conn.query(`
-    SELECT personas.primer_nombre, personas.primer_apellido,
-    personas.id as personasid,
-    usuarios.usuario, usuarios.id as usuariosid, 
-    proyectos_propuestas.id as propuestasid,
-     proyectos_propuestas.mensaje
-    FROM usuarios INNER JOIN personas ON 
-    (usuarios.persona_id=personas.id)
-    INNER JOIN trabajadores ON
-    (usuarios.id=trabajadores.usuarios_id)
-    INNER JOIN proyectos_propuestas ON
-    (proyectos_propuestas.trabajadores_id=trabajadores.id)
-    WHERE proyectos_propuestas.id=$1
-    `, [idPropuesta]);
-
-    return query.rows[0];
+    `,
+    key: 'usuario_id',
+    values: [idProyecto],
+    orderBy: 'proyectos_propuestas.id',
+    uri: '/comun/perfil/',
   });
 };
--- src/microServices/captador/getPropuestas/getPropuestasService.js
+++ src/microServices/captador/getPropuestas/getPropuestasService.js
@@ -2,83 +2,9 @@ const response = require('../../../response');
 const getPropuestasRepository = require('./getPropuestasRepository');
 
 exports.getPropuestas = async (req, res) =>{
-  const {page, perPage, idProyecto} = req.query;
-  const numberOfProposals = await getPropuestasRepository
-      .countPropuestas(idProyecto);
-
-  const HOST = req.get('host');
-  const BASE_URL = `${HOST}/captador/listar-propuestas?`;
-  const defaultLimit = 10;
-  const defaultOffset = 1;
-
-  const limit = perPage && perPage>=1? perPage:defaultLimit;
-  const offset = page && perPage>=1? page:defaultOffset;
-
-  const numberOfPages = Math.ceil(numberOfProposals/limit);
-
-  const results = [];
-
-  const getProposals = await getPropuestasRepository
-      .getPropuestas(idProyecto, limit,
-          (offset-1)*limit);
-
-  for (proposal of getProposals) {
-    results.push(formatProposal(proposal));
-  }
-
-
-  const format = {
-    pageCount: numberOfPages,
-    totalProposals: numberOfProposals,
-    // eslint-disable-next-line max-len
-    first: `${BASE_URL}idProyecto=${idProyecto}&perPage=${limit}&page=1`,
-    next: formatNext(numberOfPages, offset, BASE_URL, idProyecto, limit),
-    previous: formatPrevious(offset, BASE_URL, idProyecto, limit),
-    // eslint-disable-next-line max-len
-    last: formatLast(numberOfPages, BASE_URL, idProyecto, limit, offset),
-    results: results,
-  };
-
-  response.success(res, format);
+  response.success(res,
+      response.repage(
+          req, (await getPropuestasRepository.getPropuestas(req)),
+      ));
 };
 
-const formatNext = (numberOfPages, page, BASE_URL, idProyecto, limit) => {
-  return page >= numberOfPages? null:
-  // eslint-disable-next-line max-len
-  `${BASE_URL}idProyecto=${idProyecto}&perPage=${limit}&page=${Number(page)+1}`;
-};
-
-const formatPrevious = (page, BASE_URL, idProyecto, limit) => {
-  return page <= 1? null:
-  // eslint-disable-next-line max-len
-    `${BASE_URL}idProyecto=${idProyecto}&perPage=${limit}&page=${page-1}`;
-};
-
-const formatLast = (numberOfPages, BASE_URL, idProyecto, limit, page) => {
-  // eslint-disable-next-line max-len
-  return page===0? null:`${BASE_URL}idProyecto=${idProyecto}&perPage=${limit}&page=${numberOfPages}`;
-};
-
-const formatProposal = (row) => {
-  const apiVersion = process.env.VERSION;
-  const serverHost = process.env.HOST || 'http://localhost';
-  const serverPort = process.env.PORT || 3000;
-  const perfil = `${serverHost}:${serverPort}${apiVersion}/comun/perfil/`;
-
-  return {
-    usuario: {
-      perfil: (perfil + row.usuariosid),
-      id: row.usuariosid,
-      usuario: row.usuario,
-      persona: {
-        id: row.personasid,
-        primerNombre: row.primer_nombre,
-        primerApellido: row.primer_apellido,
-      },
-    },
-    propuesta: {
-      id: row.propuestasid,
-      mensaje: row.mensaje,
-    },
-  };
-};
